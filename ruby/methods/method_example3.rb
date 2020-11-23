05984a1e8ee8c3b8749403767bd4df9138d010a2
AMANBANDUNI <abanduni.5@gmail.com>
2020-11-23 19:47:52 +0530

    modified method eample3

--- ruby/methods/method_example3.rb
+++ ruby/methods/method_example3.rb
@@ -1,7 +1,7 @@
 # Variable number of parameters:
 
 def sample(*var)
-	puts "Total no. of plarameters are : #{var.length}"
+	puts "Total no. of parameters are : #{var.length}"
 	for i in 0...var.length
 		puts "parameter #{i+1} has value #{var[i]} "
 	end
