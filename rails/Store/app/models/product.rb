59e035e6742499cf12b2967cabb744641ea949cf
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-23 16:30:05 +0530

    added search method in the product model

--- rails/Store/app/models/product.rb
+++ rails/Store/app/models/product.rb
@@ -1,2 +1,17 @@
 class Product < ApplicationRecord
+
+  def self.search(search)
+  	if search
+  	  Product.where(["name LIKE ?","%#{search}%"])
+  	  # name_type = Product.find_by(name: search)
+  	  #   if name_type
+  	  #     Product.where(id: name_type)
+  	  #   else
+  	  #     @products = Product.all
+  	  #   end
+  	else
+  	  @products = Product.all
+  	end
+  end
+
 end
