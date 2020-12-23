43e280ce0af65257061a48c7c0561794b1b5efbc
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-23 12:18:32 +0530

    deleted aman3/

--- rails/aman3/config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  resources :products
-  root 'products#index'
-
-  get "/new1/" => "products#new1"
-  get "/new2/" => "products#new2"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
