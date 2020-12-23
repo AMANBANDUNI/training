3eaf4c595f45b267780682512ed2143decf280a4
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-23 16:28:24 +0530

    changes made in index action for search

--- rails/Store/app/controllers/products_controller.rb
+++ rails/Store/app/controllers/products_controller.rb
@@ -1,7 +1,8 @@
 class ProductsController < ApplicationController
 
   def index
-	@products = Product.all
+	  # @products = Product.all
+	  @products = Product.search(params[:search])
   end
 
   def show
@@ -13,24 +14,25 @@ class ProductsController < ApplicationController
   end
 
   def new
-	@product = Product.new
+	  @product = Product.new
   end
 
-  def new1
-	@product = Product.new
-  end
+  # def new1
+	 #  @product = Product.new
+  # end
 
-  def new2
-	@product = Product.new
-  end
+  # def new2
+	 #  @product = Product.new
+  # end
 
   def create
-	@product = Product.new(product_params)
-	if @product.save
-	  redirect_to @product, notice: "Product created successfully."
-	else
-	  render :new
-	end
+	  @product = Product.new(product_params)
+	  if @product.save
+      ProductMailer.product_confirmation.deliver
+	    redirect_to @product, notice: "Product created successfully."
+	  else
+	    render :new
+	  end
   end
 
   def update
@@ -44,14 +46,12 @@ class ProductsController < ApplicationController
 
   def destroy
   	@product = Product.find(params[:id])
-	@product.destroy
-	redirect_to products_path, notice: "Product was successfully deleted."
+	  @product.destroy
+	  redirect_to products_path, notice: "Product was successfully deleted."
   end
 
-  private
-
-    def product_params
-	  params.require(:product).permit(:name, :description, :price, :image_url)
-    end
+  def product_params
+	  params.require(:product).permit(:name, :description, :price, :image_url, :search)
+  end
 
 end
