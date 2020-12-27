bcdbfe9b88efc5c698a31346d12f24a88c953323
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-27 18:40:55 +0530

    made a new route for the form_tag to post to the multiple_destroy action

--- rails/Store/config/routes.rb
+++ rails/Store/config/routes.rb
@@ -4,5 +4,7 @@ Rails.application.routes.draw do
 
   get "/new1/" => "products#new1"
   get "/new2/" => "products#new2"
+
+  post "multiple_destroy", to: "products#multiple_destroy", as: "products_destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
