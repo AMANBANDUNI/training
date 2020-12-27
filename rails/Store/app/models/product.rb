df1f7e36d92c1bd26bd28a502ee5c27a526c7ff1
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-27 18:39:49 +0530

    Practiced some model validation and callbacks

--- rails/Store/app/models/product.rb
+++ rails/Store/app/models/product.rb
@@ -1,17 +1,53 @@
+class ImageValidator < ActiveModel::Validator
+  def validate(record)
+    if record.image_url == "evil.jpg"
+      # record.errors.add :image_url, "image name cant be evil.jpg"
+      record.errors.add(:base, 'image name cant be evil.jpg')
+    end
+  end
+end
+
+
+
+
 class Product < ApplicationRecord
+  validates :name, :price, presence: true
+  validates :price, numericality: true
+  # validates :name, uniqueness: true, on: :create
+  validates :image_url, format: {with: %r{\.(gif|jpg|png)\z}i,message: "Image is not in the .img,.png,.jpg format."}# , if: :image_is_present?
+  validates_with ImageValidator
+  validate :name_limits
+
+  before_validation :name_cant_be_empty
+
+
+  private
 
   def self.search(search)
   	if search
   	  Product.where(["name LIKE ?","%#{search}%"])
-  	  # name_type = Product.find_by(name: search)
-  	  #   if name_type
-  	  #     Product.where(id: name_type)
-  	  #   else
-  	  #     @products = Product.all
-  	  #   end
   	else
   	  @products = Product.all
   	end
   end
+
+  def name_limits
+    if (name.length < 5) || (name.length > 15) 
+      errors.add(:title, "Product name's size should be in between 5..15")
+    end
+  end
+
+  def name_cant_be_empty
+    self.name = "Product_name" if name.blank?
+  end
+
+
+  # def image_is_present?
+  #   # debugger
+  #   # errors.add(:base, 'xyz') unless image_url != "evil.jpg"
+  #   if image_url == "evil.jpg"
+  #     errors.add(:base, 'image name cant be evil.jpg')
+  #   end
+  # end
 
 end
