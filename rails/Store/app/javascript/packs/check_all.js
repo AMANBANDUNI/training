a422f4f485276e9d2764c49acf02edf6fcb9db0d
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-28 16:10:44 +0530

    corrected some missing flaws by seprating function for check_all and uncheck_all

--- rails/Store/app/javascript/packs/check_all.js
+++ rails/Store/app/javascript/packs/check_all.js
@@ -1,18 +1,30 @@
+let click = true;
 document.addEventListener('DOMContentLoaded',() => {
   console.log("Hii there");
 
-  let check = document.querySelector('#checkall');
+  
+  let check = document.querySelector('.checkall');
   check.addEventListener('click',() => {
-    check_all();
+    click? check_all() : uncheck_all();
   });
 
   function check_all(){
     console.log("hii where");
     let e = document.querySelectorAll(".mycheck");
     e.forEach((ele) => {
-      console.log("hello hii");
-      ele.checked = ele.checked ? false : true;
-      console.log('e',ele,ele.checked);
+      ele.checked = true;
+      // console.log('e',ele,ele.checked);
     })
+    click = false;
+  }
+
+  function uncheck_all(){
+    console.log("hii where");
+    let e = document.querySelectorAll(".mycheck");
+    e.forEach((ele) => {
+      ele.checked = false;
+      // console.log('e',ele,ele.checked);
+    })
+    click = true;
   }
 })
