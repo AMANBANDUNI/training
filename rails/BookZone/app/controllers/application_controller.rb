3381fe26a55a88ec942fee6515dcd9b18482a045
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-30 22:57:42 +0530

    added user_name as authentication keys

--- rails/BookZone/app/controllers/application_controller.rb
+++ rails/BookZone/app/controllers/application_controller.rb
@@ -5,7 +5,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:email,:phone_number,:password,:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:user_name,:email,:phone_number,:dob,:password,:password_confirmation])
     devise_parameter_sanitizer.permit(:account_update, keys: [:user_name,:email,:phone_number,:password,:password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:user_name,:email,:password])
   end
 end
--- rails/BookZone/app/models/user.rb
+++ rails/BookZone/app/models/user.rb
@@ -2,5 +2,8 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, authentication_keys: [:email, :user_name]
+
+  devise :omniauthable, omniauth_providers: [:twitter]
+
 end
