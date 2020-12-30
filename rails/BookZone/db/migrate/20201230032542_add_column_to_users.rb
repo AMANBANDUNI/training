c7be9973b93aa103f9006b7d47ec0b7861e8d5ab
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-30 11:33:10 +0530

    generated a new migration

--- rails/BookZone/db/migrate/20201230032542_add_column_to_users.rb
+++
@@ -1,9 +0,0 @@
-class AddColumnToUsers < ActiveRecord::Migration[6.0]
-  def change
-    add_column :users, :user_name, :string, null: false, default: ""
-    add_column :users, :phone_number, :string, default: ""
-
-    add_index :users, :user_name, 			unique: true
-    add_index :users, :phone_number,		unique: true
-  end
-end
--- rails/BookZone/db/schema.rb
+++ rails/BookZone/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_30_032542) do
+ActiveRecord::Schema.define(version: 2020_12_30_052522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,10 @@ ActiveRecord::Schema.define(version: 2020_12_30_032542) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "user_name", default: "", null: false
-    t.string "phone_number", default: ""
+    t.string "user_name"
+    t.string "phone_number"
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["user_name"], name: "index_users_on_user_name", unique: true
   end
 
 end
