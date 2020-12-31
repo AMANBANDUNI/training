c2a358e72b0c29fb9a6eb9d104c322312dd78126
AMANBANDUNI <abanduni.5@gmail.com>
2020-12-31 23:22:29 +0530

    mentioned routes for auth/:provider/callbacks

--- rails/BookZone/config/routes.rb
+++ rails/BookZone/config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   # devise_for :users
-  devise_for :users, controllers: { sessions: 'users/sessions' }
-
+  # devise_for :users, controllers: { registrations: 'users/registrations' }
+  devise_for :users, controllers: { registrations: 'users/registrations', omniauth_callbacks: "users/omniauth_callbacks"  }
+  
   root to: "home#index"
+
+  # get '/auth/:provider/callback', to: 'sessions#create'
+  # post '/auth/:provider/callback', to: 'sessions#create'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
