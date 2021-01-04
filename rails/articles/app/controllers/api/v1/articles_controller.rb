0b7bfdb7aedaaa76853e3db879fb76598f8ee292
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-04 06:08:54 +0530

    created actions for create , destroy and update

--- rails/articles/app/controllers/api/v1/articles_controller.rb
+++ rails/articles/app/controllers/api/v1/articles_controller.rb
@@ -10,6 +10,36 @@ module Api
   	  	article = Article.find(params[:id])
   	  	render json: {status: 'SUCCESS', message: 'Loaded article', data: article}, status: :ok
   	  end
+
+  	  def create
+  	  	article = Article.new(article_params)
+  	  	if article.save
+  	  	  render json: {status: 'SUCCESS', message: 'saved article', data: article}, status: :ok
+  	    else
+  	      render json: {status: 'Failure/error', message: 'article not saved', data: article.errors}, status: :unprocessable_entity
+  	    end
+  	  end
+
+  	  def destroy
+  	  	article = Article.find(params[:id])
+  	  	article.destroy
+  	  	render json: {status: 'SUCCESS', message: 'article deleted', data: article}, status: :ok
+  	  end
+
+  	  def update
+  	    article = Article.find(params[:id])
+  	    if article.update(article_params)
+  	      render json: {status: 'SUCCESS', message: 'article updated', data: article}, status: :ok
+  	    else
+  	      render json: {status: 'Failure/error', message: 'article not updated', data: article.errors}, status: :unprocessable_entity
+  	    end 
+  	  end
+
+  	  private
+
+  	  def article_params
+  	  	params.permit(:title, :body)
+  	  end
   	end
   end
 end
