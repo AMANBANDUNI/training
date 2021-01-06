da1bd5e52158e01b9e2e1c2d39b94bc3a69d79fa
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-07 03:19:14 +0530

    Created Created category and product model

--- rails/camera_stores/app/models/category.rb
+++ rails/camera_stores/app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
   has_many :products
+
+  validates :name, presence: true
+  validates :camera_type, presence: true
+  validates :model, presence: true
 end
--- rails/camera_stores/app/models/product.rb
+++ rails/camera_stores/app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ApplicationRecord
   belongs_to :category
+
+  validates :name, presence: true
+  validates :category, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :make, presence: true
 end
