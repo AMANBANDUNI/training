5ebd00edc9a1e04f9e75d22b168180fc214059d9
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-08 18:35:26 +0530

    added create_cart action to create a new cart evry time a new user sign_up

--- rails/camera_stores/app/controllers/api/v1/registrations_controller.rb
+++ rails/camera_stores/app/controllers/api/v1/registrations_controller.rb
@@ -1,20 +1,27 @@
 class Api::V1::RegistrationsController < Devise::RegistrationsController
+  # before_action :create_cart
 
   def create
     user = User.new(sign_up_params)
 
     if user.save
+      create_cart(user.id)
       token = JWT.encode({user_id: User.find_by(email: sign_up_params['email']).id}, 's3cr3t')
 
-      puts token
-      puts user.id
-      render json: token.to_json
+      # puts token
+      # puts user.id
+      # render json: token.to_json
+      render json: {status: 'SUCCESS', message: 'New User Created', data: user, token: token.to_json}, status: :ok
     else
       render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
     end
   end
 
+  def create_cart(id)
+    @cart = Cart.create(user_id: id)
+  end
 
+  private
 
   # def sign_up_params
   # 	params.require(:user).permit(:email, :password)
