efb58de86de3a774fc6737ed89e8957ec4ec8352
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-08 10:26:13 +0530

    mentioned routes for user_path

--- rails/camera_stores/config/routes.rb
+++ rails/camera_stores/config/routes.rb
@@ -14,6 +14,16 @@ Rails.application.routes.draw do
         get "/" => "home#index"
       end
 
+      get 'users/products' => 'products#index', :as => :user_products_path
+      get 'users/categories' => 'categories#index', :as => :user_categories_path
+      get 'users/:id/categories' => 'categories#show', :as => :user_category_path
+
+      get 'users/home' => 'home#show', :as => :user_path
+
+      # get 'users/home' => 'home#show', as => :user_path
+
+      # get 'users/base' => 'base#current_user', :as => :user_path
+
       # root to: "home#index"
     end
   end
