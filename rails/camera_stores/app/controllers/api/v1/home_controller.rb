10e0528ade2ed7b668b9139e4ef7d85c90391157
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-08 10:29:34 +0530

    added show action to show user details

--- rails/camera_stores/app/controllers/api/v1/home_controller.rb
+++ rails/camera_stores/app/controllers/api/v1/home_controller.rb
@@ -1,7 +1,33 @@
-class HomeController < ApplicationController
+class Api::V1::HomeController < ApplicationController
+  before_action :process_token
+
   def index
-    render json: current_user
+  	render json: current_user
+  end
+
+  def show
+  	user = User.find(@current_user_id)
+    render json: {data: user}
+    # render json: current_user.find(id: @current_user_id)
+  end
+
+  def add_product
   end	
+
+  private 
+
+  def process_token
+    if request.headers['Authorization'].present?
+      begin
+        jwt_payload = JWT.decode(request.headers['Authorization'], 's3cr3t', true, algorithm: 'HS256')
+        @current_user_id = jwt_payload[0]['user_id'].to_i
+          puts jwt_payload
+          # render 'home/index'
+      rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+        head :unauthorized
+      end
+    end
+  end
 
 
 end
