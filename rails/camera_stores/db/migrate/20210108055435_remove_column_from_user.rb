dc47cda05b7854c2ec9a374f1f4d047d0c531652
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-08 12:35:28 +0530

    Created a new Cart model

---
+++ rails/camera_stores/db/migrate/20210108055435_remove_column_from_user.rb
@@ -0,0 +1,5 @@
+class RemoveColumnFromUser < ActiveRecord::Migration[6.0]
+  def change
+  	remove_column :users, :product_id
+  end
+end
--- rails/camera_stores/db/schema.rb
+++ rails/camera_stores/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_104542) do
+ActiveRecord::Schema.define(version: 2021_01_08_055435) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -42,12 +49,10 @@ ActiveRecord::Schema.define(version: 2021_01_07_104542) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "product_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["product_id"], name: "index_users_on_product_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "categories"
-  add_foreign_key "users", "products"
 end
