b712f4eaa34d9a6abba33a0c7c66e98aac7f6117
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-08 12:36:30 +0530

    added association for cart

--- rails/camera_stores/app/models/product.rb
+++ rails/camera_stores/app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :category
-  has_and_belongs_to_many :user
+  has_and_belongs_to_many :carts
 
   validates :name, presence: true
   validates :category, presence: true
