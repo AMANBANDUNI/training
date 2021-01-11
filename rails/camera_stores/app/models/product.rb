3f5f5717a6b36a75fa3f0520d56713bd7de9325a
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-11 19:03:27 +0530

    added scope named min_price to the product model

--- rails/camera_stores/app/models/product.rb
+++ rails/camera_stores/app/models/product.rb
@@ -1,6 +1,8 @@
 class Product < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :carts
+  # scope :min_price, -> { where("price > 50000") }
+  scope :min_price, ->(amt) { where("price > ?",amt) } 
 
   validates :name, presence: true
   validates :category, presence: true
