fab6240f88d3e6854ae2a5c17f019f87058eb068
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-12 19:38:15 +0530

    added a before_create callback

--- rails/camera_stores/app/models/product.rb
+++ rails/camera_stores/app/models/product.rb
@@ -2,11 +2,22 @@ class Product < ApplicationRecord
   belongs_to :category
   has_and_belongs_to_many :carts
   # scope :min_price, -> { where("price > 50000") }
-  scope :min_price, ->(amt) { where("price > ?",amt) } 
+  scope :price_more_than, ->(amt) { where("price > ?",amt) } 
 
   validates :name, presence: true
   validates :category, presence: true
-  validates :description, presence: true
+  # validates :description, presence: true
   validates :price, presence: true
   validates :make, presence: true
+
+  before_create :increment_price
+
+  def increment_price
+  	self.price = price+10
+  end
+  
+  # before_create do
+  #   self.price = price+20  	
+  # end
+
 end
--- rails/camera_stores/app/models/user.rb
+++ rails/camera_stores/app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_one :cart
-  # scope :created_after_8, -> {where "created_at >?", DateTime.now.at_beginning_of_month + 8.day}
-  default_scope {where "created_at > ?", DateTime.now.at_beginning_of_month + 8.day} 
+  scope :created_after_8, -> {where "created_at >?", DateTime.now.at_beginning_of_month + 8.day}
+  # default_scope {where "created_at > ?", DateTime.now.at_beginning_of_month + 8.day} 
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
