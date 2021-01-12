53672d30553522fffa75c8cb0b4a1daafbc9ff52
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-12 19:31:11 +0530

    added a default scope in user model

--- rails/camera_stores/app/models/user.rb
+++ rails/camera_stores/app/models/user.rb
@@ -1,7 +1,12 @@
 class User < ApplicationRecord
   has_one :cart
+  # scope :created_after_8, -> {where "created_at >?", DateTime.now.at_beginning_of_month + 8.day}
+  default_scope {where "created_at > ?", DateTime.now.at_beginning_of_month + 8.day} 
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  
 end
