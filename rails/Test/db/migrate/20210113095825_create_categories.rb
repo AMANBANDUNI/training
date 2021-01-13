117061f0caeddeee49a2e378b7aad62042b0a234
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-13 18:02:27 +0530

    Created a new migration to create categories table

--- rails/Test/db/migrate/20210113095825_create_categories.rb
+++ rails/Test/db/migrate/20210113095825_create_categories.rb
@@ -2,8 +2,8 @@ class CreateCategories < ActiveRecord::Migration[6.0]
   def change
     create_table :categories do |t|
       t.string :name, null: false
-      t.text :camera_type, default: "Undecided", null: false
-      t.integer :model, null:false
+      t.string :camera_type, default: "Undecided", null: false
+      t.integer :model, null:false, default: 2015
 
       t.timestamps
     end
