dcd0be2134b2dfcea26e3b321cb6834dd8907a16
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-14 17:58:13 +0530

    added a custom route for index action of home controller

--- rails/Test/config/routes.rb
+++ rails/Test/config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :home, only: [:index,:create]
+  # resources :home, only: [:index,:create]
+  get 'products', to: "home#index", as: 'products_path'
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
