b3364460b525c06e25b4954d9f3823f37b42522e
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-14 16:57:19 +0530

    added association among cart and products , user and cart

--- rails/Test/db/schema.rb
+++ rails/Test/db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_100246) do
+ActiveRecord::Schema.define(version: 2021_01_14_101434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "carts", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_carts_on_user_id"
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -42,5 +49,6 @@ ActiveRecord::Schema.define(version: 2021_01_14_100246) do
     t.string "confirmpassword", null: false
   end
 
+  add_foreign_key "carts", "users"
   add_foreign_key "products", "categories"
 end
