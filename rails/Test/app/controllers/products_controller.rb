bf1d0e01fd55dd00cfc37618f64dd19ed5e01741
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-15 15:10:25 +0530

    in create action redirected to preview_product_path

--- rails/Test/app/controllers/products_controller.rb
+++ rails/Test/app/controllers/products_controller.rb
@@ -7,16 +7,31 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
   end
 
+  def new
+  	@product = Product.new
+  	# debugger
+  end
+
   def create
+  	# debugger
+  	@product = Product.new(product_params)
+  	if @product.save!
+  	  # debugger
+  	  redirect_to preview_product_path(@product.id), notice: "New Product Created succefully."
+  	else
+  	  render :new
+  	end
   end
 
   def preview 
+  	@product = Product.find(params[:id])
   end
 
   private
 
   def product_params
-  	params.permit(:name)
+  	# params.permit(:name,:description,:price,:make, :category_id)
+  	params.require(:product).permit(:name,:description,:price,:make, :category_id)
   end
 
 end
