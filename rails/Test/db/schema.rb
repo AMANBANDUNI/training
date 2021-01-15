7cd6245f5bd27da07e67fd1f0de9551c9b670906
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-15 17:01:00 +0530

    Created a join table for carts_products

--- rails/Test/db/schema.rb
+++ rails/Test/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_14_101434) do
+ActiveRecord::Schema.define(version: 2021_01_15_111946) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,13 @@ ActiveRecord::Schema.define(version: 2021_01_14_101434) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "carts_products", id: false, force: :cascade do |t|
+    t.bigint "cart_id", null: false
+    t.bigint "product_id", null: false
+    t.index ["cart_id", "product_id"], name: "index_carts_products_on_cart_id_and_product_id"
+    t.index ["product_id", "cart_id"], name: "index_carts_products_on_product_id_and_cart_id"
   end
 
   create_table "categories", force: :cascade do |t|
