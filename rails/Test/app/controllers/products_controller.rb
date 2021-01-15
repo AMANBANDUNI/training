2dced57ebb50064d4a90e4662dfd88f07419f0e3
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-15 17:13:46 +0530

    Created edit.html.erb view and rendered form partial , created actions for edit , update, destroy, created link for show, edit and delete

--- rails/Test/app/controllers/products_controller.rb
+++ rails/Test/app/controllers/products_controller.rb
@@ -14,9 +14,16 @@ class ProductsController < ApplicationController
 
   def create
   	# debugger
-  	@product = Product.new(product_params)
-  	if @product.save!
-  	  # debugger
+  	# @product = Product.new(product_params)
+  	# if @product.save!
+  	#   # debugger
+  	#   redirect_to preview_product_path(@product.id), notice: "New Product Created succefully."
+  	# else
+  	#   render :new
+  	# end
+
+  	if Product.create!(product_params)
+  	  @product = Product.last
   	  redirect_to preview_product_path(@product.id), notice: "New Product Created succefully."
   	else
   	  render :new
@@ -25,6 +32,25 @@ class ProductsController < ApplicationController
 
   def preview 
   	@product = Product.find(params[:id])
+  end
+
+  def edit
+  	@product = Product.find(params[:id])
+  end
+
+  def update
+  	@product = Product.find(params[:id])
+  	if @product.update(product_params)
+  	  # debugger
+  	  redirect_to preview_product_path(@product.id), notice: "Product updated succefully."
+  	else
+  	  render :edit
+  	end
+  end
+
+  def destroy
+  	Product.find(params[:id]).destroy
+  	redirect_to products_path, notice: "1 Product destroyed successfully"
   end
 
   private
