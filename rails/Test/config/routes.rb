b024b1623b24fef434a9f6eab260f5a24fc59101
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-16 00:52:44 +0530

    mentioned routes for the multiple_destroy action through the collection function in the resources

--- rails/Test/config/routes.rb
+++ rails/Test/config/routes.rb
@@ -12,6 +12,9 @@ Rails.application.routes.draw do
   	member do
   	  get :preview
   	end
+    collection do
+      post :multiple_destroy
+    end
   end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
