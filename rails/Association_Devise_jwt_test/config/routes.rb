ab8d96cc6da7067e80da613e1f03d7b70a63f02e
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-18 16:49:41 +0530

    metioned routes for the api/v1 controllers and for some custom actions

--- rails/Association_Devise_jwt_test/config/routes.rb
+++ rails/Association_Devise_jwt_test/config/routes.rb
@@ -5,5 +5,26 @@ Rails.application.routes.draw do
   }
 
   root to: "home#index"
+
+  namespace 'api' do 
+    namespace 'v1' do 
+      resources :images
+
+      devise_for :users,
+      controllers: {
+        registrations: 		'api/v1/registrations',
+        sessions:         'api/v1/sessions'
+      }
+
+      devise_scope :user do
+        get "/" => "home#index"
+      end
+
+      get 'users/home', to: 'home#show', as: 'user_path'
+      
+
+    end
+  end
+  get 'user/:id/images/', to: 'home#show_images', as: "user_images"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
