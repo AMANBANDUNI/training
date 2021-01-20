b818bd01a46bae664ee82eea7ea765a6113354ae
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-21 05:19:32 +0530

    Called the UserSerializer and parse the user object

--- rails/Association_Devise_jwt_test/app/controllers/api/v1/sessions_controller.rb
+++ rails/Association_Devise_jwt_test/app/controllers/api/v1/sessions_controller.rb
@@ -7,7 +7,7 @@ class Api::V1::SessionsController < Devise::SessionsController
     # debugger
     if user && user.valid_password?(sign_in_params[:password])
   	  token = JWT.encode({user_id: user.id}, 's3cr3t')
-  	  render json: {status: 'SUCCESS', message: 'signed in ', data: user, token: token.to_json}, status: :ok
+  	  render json: {status: 'SUCCESS', message: 'signed in ', user: UserSerializer.new(user), token: token.to_json}, status: :ok
   	else
   	  render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
   	end
