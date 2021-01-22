f6eb0807a3c87d6fb69496239b6d88a58fabaaf5
AMANBANDUNI <abanduni.5@gmail.com>
2021-01-22 10:22:09 +0530

    metioned routes for the api actions

--- rails/ContactBook/config/routes.rb
+++ rails/ContactBook/config/routes.rb
@@ -6,5 +6,18 @@ Rails.application.routes.draw do
     registrations: 	'users/registrations'
   }
   root to: "home#index"
+
+  namespace 'api' do 
+  	namespace 'v1' do
+  	  devise_for :users,
+      controllers: {
+        registrations: 		'api/v1/registrations',
+        sessions:         'api/v1/sessions'
+      }
+      devise_scope :user do
+        get "/" => "home#index"
+      end
+  	end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
